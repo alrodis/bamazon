c5b16402cbd6445e85622bc5f41a8185e5f422d6
Alvin Rodis <alrodis147@gmail.com>
2017-11-15 17:30:06 -0600

    11-15 commit

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -26,14 +26,13 @@ function displayAll() {
             console.log("ID | Product | Department | Price")
             console.log(res[i].item_id + " | " + res[i].product_name + " | " + res[i].department_name + " | " + res[i].price);
         };
-        //once all items are displayed, user can then make a purchase 
+        //once all items are displayed, user can then make a purchase, calling start function
         start();
     });
 }
 
 
 function start() {
-    //console.log("Started");
     connection.query("SELECT * FROM products", function(err, res) {
         // console.log(res);
         //prompting user with two messages: what product they'd like to buy and how many units of each would they like to purchase
@@ -55,10 +54,9 @@ function start() {
                     }
                 }
             ])
-            //all processing is being done inside the .then
             .then(function(answer) {
                 //incremental console logging to confirm I can grab the needed information
-                
+
                 // console.log("The user has selected item_id (productId): " + answer.productId);
                 // console.log("The user wants to buy " + answer.purchaseQuantity + " unit(s) of this item");
                 // console.log("Product Name: " + res[4].product_name);
@@ -67,51 +65,42 @@ function start() {
 
                 var userItem = answer.productId;
                 order(userItem, parseInt(answer.purchaseQuantity));
-                // var userQuantity = answer.purchaseQuantity;
-
 
             })
 
-
     })
-
-
 
 }; //closes the start function...
 
 
 function order(productId, quantity) {
-	// console.log('product ID -----', productId);
+    // console.log('product ID -----', productId);
     connection.query("SELECT * FROM products WHERE item_id =" + productId, function(err, res) {
         if (err) throw err;
         var availableQuantity = res[0].stock_quantity;
         var currentPrice = parseInt(res[0].price);
 
-
-        //since I couldn't figure out how to grab the user input product selection, I've hard coded an item for now (Levi's Jeans)
         //first step is to confirm the customer can actually place an order, based upon checking the total quantity in stock vs. the amount the user would like to purchase
         if (quantity <= availableQuantity) {
-        	//console.log('this is the item from DB', res[0]);
+            //console.log('this is the item from DB', res[0]);
             //calculating the new depricated stock quantity, based upon the user input value of how many they want to purchase
             var newStockQuantity = (availableQuantity - quantity);
             // console.log("New stock quantity= " + newStockQuantity);
 
             //console.log('this is availableQuantity ----', availableQuantity);
             //console.log('this is quantity ----', quantity);
-            // var query = connection.query(
             connection.query("UPDATE products SET ? WHERE ?", [{
-                        stock_quantity: newStockQuantity
-                    },
-                    {
-                        item_id: productId
-                    }
-                ]
-            );
+                    stock_quantity: newStockQuantity
+                },
+                {
+                    item_id: productId
+                }
+            ]);
 
             //calculating the total cost of the order, based upon the quantity the user wants to purchase
             var totalCost = (quantity * currentPrice);
             // console.log(totalCost);
-            //now will give the user the total cost of their order
+            //now we will give the user the total cost of their order
             console.log("Thank you for your order.  Your total cost is: " + totalCost);
         } else {
             //if there is insufficient quantity in stock, the order will not go through and the user will go back to the start to make a new selection
